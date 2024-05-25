95b93275d37073dcf1795e64db341e33de312c96
Kasper Timm Hansen <kaspth@gmail.com>
2024-05-25 18:48:33 -0500

    Readd ActionView::Partial and also add ActionView::Element

--- app/views/tab_nav/bar.rb
+++ app/views/tab_nav/bar.rb
@@ -1,35 +1,31 @@
-class TabNav::Bar < Struct.new(:view)
-  def self.render_in(view, &) = new(view).render(&)
-
-  def render(&)
-    view.render "tab_nav/bar", tabs: view.capture(self, &), extra:
-  end
-  def extra(&) = @extra ||= view.capture(&)
+class TabNav::Bar < ActionView::Partial
+  slot :extra
+  def render(&) = super("tab_nav/bar", tabs: capture(&), extra:)
 
   def link_tab(text, link, icon:, selected: view.current_page?(link), counter: nil, threshold: nil)
-    tab(text:, icon:, selected:).partial __method__, link:, counter: (Counter.new(view, counter, threshold) if counter)
+    Tab.new(text:, icon:, selected:).partial __method__, link:, counter: (Counter.new(counter, threshold) if counter)
   end
 
   def disabled_tab(text, icon:, tooltip:)
-    tab(text:, icon:, disabled: true).partial __method__, tooltip:
+    Tab.new(text:, icon:, disabled: true).partial __method__, tooltip:
   end
 
   def dropdown_tab(text, icon:, selected: false, &)
-    tab(text:, icon:, selected:).partial __method__, items: Dropdown.captured(view, &)
+    Tab.new(text:, icon:, selected:).partial __method__, items: Dropdown.capture(&)
   end
 
   private
-    def tab(**) = Tab.new(view:, **)
+    class Tab < Data.define(:text, :icon, :disabled, :selected)
+      include ActionView::Element
 
-    class Tab < Data.define(:view, :text, :icon, :disabled, :selected)
       alias disabled? disabled
       alias selected? selected
-      def initialize(view:, text:, icon: nil, disabled: false, selected: false) = super
+      def initialize(text:, icon: nil, disabled: false, selected: false) = super
 
-      def partial(key, **) = view.render("tab_nav/#{key}", tab: self, **)
+      def partial(key, **) = render("tab_nav/#{key}", tab: self, **)
 
       def classes
-        view.class_names(
+        class_names(
           "group whitespace-nowrap flex items-center space-x-1 rounded rounded-b-none leading-none py-3 px-3 border",
           "border-gray-300 border-b-gray-400 hover:bg-gray-50":  !disabled?,
           "border-gray-200 border-b-gray-400 cursor-not-allowed": disabled?,
@@ -42,13 +38,13 @@ class TabNav::Bar < Struct.new(:view)
       end
 
       def text(**)
-        view.tag.span(super(), class: ["text-gray-500 group-hover:text-gray-600", "font-semibold": selected?], **)
+        tag.span(super(), class: ["text-gray-500 group-hover:text-gray-600", "font-semibold": selected?], **)
       end
     end
 
-    class Counter < Data.define(:view, :counter, :threshold)
+    class Counter < Data.define(:counter, :threshold)
       def to_s
-        view.tag.span counter, class: ["text-xs leading-none p-1 rounded bg-gray-200 text-gray-600", threshold_classes]
+        ActionView::Element.tag.span counter, class: ["text-xs leading-none p-1 rounded bg-gray-200 text-gray-600", threshold_classes]
       end
 
       private
@@ -61,11 +57,9 @@ class TabNav::Bar < Struct.new(:view)
         end
     end
 
-    class Dropdown < Data.define(:view)
-      def self.captured(view, &) = view.capture(new(view), &)
-
+    class Dropdown < ActionView::Partial
       def item(text, link)
-        view.link_to text, link, class: "block w-full text-left py-1.5 px-3 text-gray-500 hover:text-gray-600 hover:bg-gray-50"
+        link_to text, link, class: "block w-full text-left py-1.5 px-3 text-gray-500 hover:text-gray-600 hover:bg-gray-50"
       end
     end
 end
